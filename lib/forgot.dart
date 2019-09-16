8aec92f191f5e3087ffc3e2f4d21ced92b1a73c0
Suresh Shetty <popcap.suresh@gmail.com>
2019-09-16 15:38:27 +0545

    updted

---
+++ lib/forgot.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Forgot",
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: MyButton(),
+    );
+  }
+}
+
+class MyButton extends StatefulWidget {
+  MyButton({Key key}) : super(key: key);
+
+  _MyButtonState createState() => _MyButtonState();
+}
+
+class _MyButtonState extends State<MyButton> {
+  TextEditingController _textFieldController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+        title: Text("Forgot Password"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Center(
+              child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 25.0),
+            child: TextField(
+              controller: _textFieldController,
+              decoration: InputDecoration(
+                
+                labelText: " Phone Number",
+               
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+            ),
+          ),
+          ),
+          Container(
+        margin: EdgeInsets.only(top: 20.0),
+        width: 200.0,
+        height: 50.0,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20.0)
+        ),
+        child: RaisedButton(
+          
+            color: Colors.blueAccent,
+            child: Text(
+              "Continue",
+              style: TextStyle(color: Colors.white),
+              
+            ),
+            
+            elevation: 6.0,
+            onPressed: () => (context)),
+      )
+        ],
+        
+      ),
+    );
+
+    }
+  }
+
+  
