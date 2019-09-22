bf963f33e3b84e51d732a754ebde3e26f321311f
Suresh Shetty <popcap.suresh@gmail.com>
2019-09-22 15:16:29 +0545

    text message

--- lib/main.dart
+++ lib/main.dart
@@ -2,75 +2,82 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(App());
 
-class App extends StatelessWidget{
+class App extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: Color(0xFFFB415B),
-        
       ),
       home: LoginPage(),
     );
   }
 }
 
-class LoginPage extends StatefulWidget{
+class LoginPage extends StatefulWidget {
   @override
   _LoginPageState createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage>{
-
+class _LoginPageState extends State<LoginPage> {
   bool _isHidden = true;
+  bool _second = true;
 
-  void _toggleVisibility(){
+  void _toggleVisibility() {
     setState(() {
       _isHidden = !_isHidden;
     });
   }
 
+  void _another() {
+    setState(() {
+      _second = !_second;
+    });
+  }
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blueAccent,
-        title:Text("Reset Password"
-        )),
       resizeToAvoidBottomPadding: false,
       body: Container(
-        padding: EdgeInsets.only(top: 100.0, right: 20.0, left: 20.0, bottom: 20.0),
+        padding:
+            EdgeInsets.only(top: 100.0, right: 20.0, left: 20.0, bottom: 20.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            
-            SizedBox(height: 40.0,),
+            top(),
+            SizedBox(
+              height: 40.0,
+            ),
             buildTextField("New Password"),
-            SizedBox(height: 20.0,),
-            buildTextField("Confirm Password"),
-            SizedBox(height: 20.0,),
+            SizedBox(
+              height: 20.0,
+            ),
+            buildTextFieldtwo("Confirm Password"),
+            SizedBox(
+              height: 20.0,
+            ),
             Container(
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.end,
-            
               ),
             ),
             SizedBox(height: 50.0),
             buildButtonContainer(),
-            SizedBox(height: 10.0,),
-            
+            SizedBox(
+              height: 10.0,
+            ),
           ],
         ),
       ),
     );
   }
 
-  Widget buildTextField(String hintText){
+  Widget buildTextField(String labelText) {
     return TextField(
       decoration: InputDecoration(
-    
-        hintText: hintText,
+        labelText: labelText,
         hintStyle: TextStyle(
           color: Colors.grey,
           fontSize: 16.0,
@@ -78,23 +85,71 @@ class _LoginPageState extends State<LoginPage>{
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(10.0),
         ),
-     
-        suffixIcon: hintText == "Password" ? IconButton( 
-          onPressed: _toggleVisibility,
-          icon: _isHidden ? Icon(Icons.visibility_off) : Icon(Icons.visibility),
-        ) : null,
+        suffixIcon: labelText == "New Password"
+            ? IconButton(
+                onPressed: _toggleVisibility,
+                icon: _isHidden
+                    ? Icon(Icons.visibility_off)
+                    : Icon(Icons.visibility),
+              )
+            : null,
       ),
-      obscureText: hintText == "Password" ? _isHidden : false,
+      obscureText: !_isHidden,
     );
   }
 
-  Widget buildButtonContainer(){
+  Widget buildTextFieldtwo(String labelText) {
+    return TextField(
+      decoration: InputDecoration(
+        labelText: labelText,
+        hintStyle: TextStyle(
+          color: Colors.grey,
+          fontSize: 16.0,
+        ),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        suffixIcon: labelText == "Confirm Password"
+            ? IconButton(
+                onPressed: _another,
+                icon: _second
+                    ? Icon(Icons.visibility_off)
+                    : Icon(Icons.visibility),
+              )
+            : null,
+      ),
+      obscureText: !_second,
+    );
+  }
+
+  Widget top() {
     return Container(
-      height: 56.0,
-      width: MediaQuery.of(context).size.width,
+        child: Column(
+      children: <Widget>[
+        Text(
+          "Reset Password",
+          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+        ),
+        Center(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 30.0, left: 20.0),
+            child: Text(
+              "A minimum 8 characters password contains a combination of uppercase and lowercase letter and number are required.",
+              style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+      ],
+    ));
+  }
+
+  Widget buildButtonContainer() {
+    return Container(
+      height: MediaQuery.of(context).size.height / 10,
+      width: MediaQuery.of(context).size.width / 1,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(20.0),
-        color: Colors.blueAccent,
+        color: Colors.blue[900],
       ),
       child: Center(
         child: Text(
@@ -106,6 +161,5 @@ class _LoginPageState extends State<LoginPage>{
         ),
       ),
     );
-
   }
 }
