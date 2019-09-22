b0a32e422cad43b359c5fe45cf87dca8ec02c8f3
Suresh Shetty <popcap.suresh@gmail.com>
2019-09-22 15:53:50 +0545

    change

--- lib/forgot.dart
+++ lib/forgot.dart
@@ -27,57 +27,52 @@ class _MyButtonState extends State<MyButton> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
-      
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           Center(
-              child: Text("Forgot Password",
-               style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)
-              ),
+            child: Text("Forgot Password",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
           ),
           Center(
-              child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 25.0),
-            child: TextField(
-              controller: _textFieldController,
-              decoration: InputDecoration(
-                
-                labelText: " Phone Number",
-               
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10.0),
+            child: Padding(
+              padding: const EdgeInsets.only(top: 30.0, left: 20.0),
+              child: Text("Enter your phone number",
+                  style:
+                      TextStyle(fontSize: 10.0, fontWeight: FontWeight.bold)),
+            ),
+          ),
+          Center(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 25.0),
+              child: TextField(
+                controller: _textFieldController,
+                decoration: InputDecoration(
+                  labelText: " Phone Number",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
                 ),
               ),
             ),
           ),
-          ),
           Container(
-        margin: EdgeInsets.only(top: 20.0),
-        width: 200.0,
-        height: 50.0,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20.0)
-        ),
-        child: RaisedButton(
-          
-            color: Colors.blueAccent,
-            child: Text(
-              "Continue",
-              style: TextStyle(color: Colors.white),
-              
-            ),
-            
-            elevation: 6.0,
-            onPressed: () => (context)),
-      )
+            margin: EdgeInsets.only(top: 20.0),
+            height: MediaQuery.of(context).size.height / 10,
+            width: MediaQuery.of(context).size.width / 1,
+            decoration:
+                BoxDecoration(borderRadius: BorderRadius.circular(20.0)),
+            child: RaisedButton(
+                color: Colors.blue[900],
+                child: Text(
+                  "Continue",
+                  style: TextStyle(color: Colors.white),
+                ),
+                elevation: 6.0,
+                onPressed: () => (context)),
+          )
         ],
-        
       ),
     );
-
-    }
   }
-
-  
+}
