693f8a94d0ef64a3f57b326266afc2040210495c
xaugat <sauugat@gmail.com>
2019-10-17 12:28:01 +0545

    saugat again commited

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -3,10 +3,14 @@
 //
 
 #import "GeneratedPluginRegistrant.h"
+#import <image_picker/ImagePickerPlugin.h>
+#import <url_launcher/UrlLauncherPlugin.h>
 
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTImagePickerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTImagePickerPlugin"]];
+  [FLTUrlLauncherPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTUrlLauncherPlugin"]];
 }
 
 @end
--- lib/userprofile.dart
+++ lib/userprofile.dart
@@ -1,63 +1,104 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:async';
+import 'dart:io';
 
 
-class Userprofile extends StatelessWidget{
-  @override 
-  Widget build(BuildContext context){
+class Userprofile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: new UserProfile(),
-  );
-
+    );
   }
-  
 }
 
-class UserProfile extends StatefulWidget{
-
- @override
+class UserProfile extends StatefulWidget {
+  @override
   State createState() => new UserProfileState();
 }
 
-class UserProfileState extends State<UserProfile>{
-  @override 
-  Widget build (BuildContext context){
+class UserProfileState extends State<UserProfile> {
+  File _image;
+
+  Future getImage() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      _image = image;
+      
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("My Profile"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.list),
+            onPressed: (){
+            },
+          ),
+        ],),
       body: Container(
         padding: EdgeInsets.all(40),
-        
         child: Stack(
           children: <Widget>[
-           
-            Text("Welcome User", textAlign: TextAlign.center,),
-            
             userimg(),
+                sizeheight(200),
+            Container(
+              margin: EdgeInsets.only(top: 60, left: 200, right: 100),
+      child: FloatingActionButton(
+      onPressed: getImage,
+      tooltip: 'Pick Image',
+      child: Icon(Icons.add_a_photo),
+    ),),
+    Container(
+      margin: EdgeInsets.only(top: 300, left: 100),
+      child: Text(
+      'saugat'
+      ),
+    ),
+
           ],
+          
         ),
       ),
-
-    );
-  } 
-}
-
-userimg(){
-  return Container(
-  margin: EdgeInsets.only(top: 100, left: 50),
-   alignment: Alignment.center,
-                            width: 200.0,
-                            height: 200.0,
-                            
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(100.0),
-                              border: Border.all(color: Colors.blue),
-                              
-                              
-                            ),
-                            child: Text("Your Photo"),
-
-    );
-}
-SizedBox sizewidth(double b) {
-    return SizedBox(
-      width: b,
     );
   }
+
+
+userimg() {
+  return Container(
+    margin: EdgeInsets.only(top: 60, left: 50),
+    alignment: Alignment.center,
+    width: 200.0,
+    height: 200.0,
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(10.0),
+      border: Border.all(color: Colors.blue),
+      boxShadow: [new BoxShadow(
+            color: Colors.black,
+            blurRadius: 5.0,
+          ),]
+    ),
+    child: _image == null ? Text('Add your photo',style: TextStyle(color: Colors.white.withOpacity(0.6)),) : Image.file(_image),
+  );
+}
+}
+
+SizedBox sizeheight(double a) {
+    return SizedBox(
+      height: a,
+    );
+  }
+
+SizedBox sizewidth(double b) {
+  return SizedBox(
+    width: b,
+  );
+}
+
+
